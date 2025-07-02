cc762377a7848cdb16ddc965c0c61989e74fd7ee
JosueAcostaM <josuem3693@gmail.com>
2025-07-01 19:45:34 -0500

    Cambio para crear la rama

---
+++ Vivelab.Modelos/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Vivelab.Modelos
+{
+    public class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Clase extra solo para actualizar mi rama");
+        }
+    }
+}
