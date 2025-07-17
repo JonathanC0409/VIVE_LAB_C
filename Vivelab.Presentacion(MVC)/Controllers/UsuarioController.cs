1e1a60c0676b0c1541bab0221adc0e4a658f5f0d
JonathanC0409 <soyeljoni123@gmail.com>
2025-07-16 23:45:51 -0500

    metodo buscar cancion

--- Vivelab.Presentacion(MVC)/Controllers/UsuarioController.cs
+++ Vivelab.Presentacion(MVC)/Controllers/UsuarioController.cs
@@ -513,6 +513,29 @@ namespace Vivelab.Presentacion_MVC_.Controllers
             return RedirectToAction("VerCanciones", new { albumId = albumId });  // Redirigir a la vista de canciones del álbum
         }
 
+        [HttpPost]
+        public IActionResult SearchSong(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                // Si no se proporciona un nombre, devolver una vista con un mensaje de error o realizar otra acción.
+                ViewBag.ErrorMessage = "Por favor, ingresa un nombre de canción.";
+                return View();
+            }
+
+ 
+            var canciones = CRUD<Cancion>.GetAll(); // Obtener todas las canciones desde el CRUD
+            canciones = canciones.Where(c => c.Titulo.ToLower().Contains(name.ToLower())).ToList();
+
+
+            if (canciones.Count == 0)
+            {
+                ViewBag.ErrorMessage = "No se encontraron canciones con ese nombre.";
+            }
+
+            return View(canciones); // Devuelves la lista de canciones encontradas o el mensaje de error.
+        }
+
 
     }
 }
