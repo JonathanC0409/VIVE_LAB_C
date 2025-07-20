f582041f50ce4f53a4a929cf3d4614846c5eebd1
JonathanC0409 <soyeljoni123@gmail.com>
2025-07-20 17:23:44 -0500

    vista registro mejorada

--- Vivelab.Presentacion(MVC)/Controllers/LoginController.cs
+++ Vivelab.Presentacion(MVC)/Controllers/LoginController.cs
@@ -56,7 +56,7 @@ namespace Vivelab.Presentacion_MVC_.Controllers
             else
             {
                 // Mostrar mensaje de error
-                ViewBag.ErrorMessage = "Error al registrar el usuario.";
+                ViewBag.ErrorMessage = "El correro ya esta registrado";
                 return View();
             }
         }
